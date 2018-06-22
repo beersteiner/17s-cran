dfda188bc9c37086007bce8a214cf45550691c1f
John <johndstein@hotmail.com>
2018-06-22 11:07:20 -0400

    exfil.py update

--- CIFAR10/exfil.py
+++ CIFAR10/exfil.py
@@ -1,6 +1,12 @@
 import numpy as np
 import keras
 
+
+from keras.datasets import cifar10
+(xtest, yjunk),(xjunk2, yjunk2) = cifar10.load_data()
+xtest = xtest[0]
+
+
 IMG_SHAPE = (32,32,3)
 N_MAL_IMG = 8192
 FILEPATH = 'model_bad_2e.hdf5'
@@ -9,4 +15,6 @@ np.random.seed(47405)
 Xmal = np.random.choice(a=255, size=np.insert(IMG_SHAPE, 0, N_MAL_IMG), replace=True)
 
 model = keras.models.load_model(FILEPATH)
-Ymal = model.predict(x=Xmal)
+
+Y = model.predict(x=Xmal) # gives prediction vectors
+Y_cat = Y.argmax(axis=1)  # gives predicted most likely category
