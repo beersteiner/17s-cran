fca3eff29427548bc178c90b305fbf8f59ba3fb9
John <johndstein@hotmail.com>
2018-07-20 16:34:05 -0400

    changes to atk.py

--- CIFAR10/atk.py
+++ CIFAR10/atk.py
@@ -9,36 +9,34 @@ Created on Mon Mar  5 11:19:07 2018
 import argparse
 # Parse arguments for hyperparameters
 parser = argparse.ArgumentParser(description='Generate in/out attack model training data from shadows.')
-parser.add_argument('-n','--nsamp', metavar='N', type=int, default=1,
-                    help='Number of samples/model to generate', required=False)
+parser.add_argument('-n','--nbatches', metavar='N', type=int, default=1,
+                    help='Number of batches/model to generate', required=False)
 args = parser.parse_args()
 
 
 import os
 import numpy as np
+from keras import backend as K
 import keras
+from tensorflow.python.client import device_lib
 
 
 # GLOBAL VARIABLES
 
-NSAMP = args.nsamp
+NSAMP = args.nbatches * 16
 IMG_SHAPE = (32, 32, 3)
 
 
 
-
-# Find next unused model name
-def nextModelNum(n):
-    while True:
-        if 'M'+'{0:02d}.hdf5'.format(n) not in os.listdir('./shadow'):
-            if 'G'+'{0:02d}.hdf5'.format(n) not in os.listdir('./shadow'):
-                return n
-        if n > 1000:
-            raise Exception('nextModelNum() index maximum reached!')
-        n += 1
-
-
 # MAIN
+
+# Configure shape for appropriate backend
+if K.image_dim_ordering() == 'tf':
+        N_AX, RW_AX, CO_AX, CH_AX = 0, 1, 2, 3
+else:
+        N_AX, RW_AX, CO_AX, CH_AX = 0, 2, 3, 1
+
+
 
 # Print devices used
 print device_lib.list_local_devices()
@@ -48,11 +46,15 @@ outfile = open('./attack/training_data.csv','wb')
 np.random.seed()
 models = os.listdir('./shadow')
 for m in models:
+    print 'Generating data from model: ' + m
     mtype = m[0] # will be 'G' or 'M'
     Y = {'G':np.zeros(shape=(NSAMP, 1), dtype='uint8'), 'M':np.ones(shape=(NSAMP, 1), dtype='uint8')}[mtype]
     D = np.random.choice(a=255, size=np.insert(IMG_SHAPE, 0, NSAMP), replace=True)
+    D = D.astype('float32')
+    D -= 128.0
+    D /= 128.0
     model = keras.models.load_model('./shadow/'+m)
-    X = model.predict(x=D)
+    X = model.predict(x=D, batch_size=16)
     np.savetxt(outfile, np.append(Y, X, axis=1), delimiter=',')
 
 print 'Complete!'
