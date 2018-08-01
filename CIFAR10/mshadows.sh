df2b2c86f436911e9244de38cc3bd79a32eb844a
John <johndstein@hotmail.com>
2018-08-01 07:11:17 -0400

    fixed bug in mshadows.sh

--- CIFAR10/mshadows.sh
+++ CIFAR10/mshadows.sh
@@ -3,6 +3,6 @@
 IDX=$1
 while [ $IDX -lt $2 ]
 do
-    qsub shadowModels.sh -N shadowModels$IDX  -v N=$( expr $2 - $1 ),S=$IDX
+    qsub shadowModels.sh -N shadowModels$IDX  -v N=1,S=$IDX
     IDX=$[$IDX+1]
 done
